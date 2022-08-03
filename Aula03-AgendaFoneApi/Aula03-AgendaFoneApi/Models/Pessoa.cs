b559e63e08f515f962d964afd542c1f79544d106
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2022-08-02 22:59:43 -0300

    Aula03-AgendaFoneApi

---
+++ Aula03-AgendaFoneApi/Aula03-AgendaFoneApi/Models/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿namespace Aula03_AgendaFoneApi.Models;
+
+public class Pessoa : Entidade
+{
+    public Pessoa(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+
+    public string Nome { get; private set; }
+    public IList<Telefone> Telefones { get; private set; }
+}
